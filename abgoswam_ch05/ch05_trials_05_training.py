937cdca8a0d7c599e776d91a1575b84d3bcf13a9
Abhishek Goswami <agoswami@microsoft.com>
2025-07-22 15:53:07 -0700

    done ch05

--- abgoswam_ch05/ch05_trials_05_training.py
+++ abgoswam_ch05/ch05_trials_05_training.py
@@ -1,6 +1,9 @@
+import tiktoken
 import torch
+import os
+import urllib.request
 
-from previous_chapters import GPT_CONFIG_124M, GPTModel, generate_text_simple
+from previous_chapters import GPTModel, create_dataloader_v1, generate_text_simple
 
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch, target_batch = input_batch.to(device), target_batch.to(device)
@@ -49,8 +52,10 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
     encoded = text_to_token_ids(start_context, tokenizer).to(device)
     with torch.no_grad():
         token_ids = generate_text_simple(
-            model=model, idx=encoded,
-            max_new_tokens=50, context_size=context_size
+            model=model, 
+            idx=encoded,
+            max_new_tokens=50, 
+            context_size=context_size
         )
     decoded_text = token_ids_to_text(token_ids, tokenizer)
     print(decoded_text.replace("\n", " "))  # Compact print format
@@ -95,33 +100,89 @@ if __name__ == "__main__":
 
     # Note:
     # Uncomment the following code to calculate the execution time
-    # import time
-    # start_time = time.time()
-
-    # GPT_CONFIG_124M = {
-    #     "vocab_size": 50257,   # Vocabulary size
-    #     "context_length": 256, # Shortened context length (orig: 1024)
-    #     "emb_dim": 768,        # Embedding dimension
-    #     "n_heads": 12,         # Number of attention heads
-    #     "n_layers": 12,        # Number of layers
-    #     "drop_rate": 0.1,      # Dropout rate
-    #     "qkv_bias": False      # Query-key-value bias
-    # }
+    import time
+    start_time = time.time()
 
     torch.manual_seed(123)
+
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,   # Vocabulary size
+        "context_length": 256, # Shortened context length (orig: 1024)
+        "emb_dim": 768,        # Embedding dimension
+        "n_heads": 12,         # Number of attention heads
+        "n_layers": 12,        # Number of layers
+        "drop_rate": 0.1,      # Dropout rate
+        "qkv_bias": False      # Query-key-value bias
+    }
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    ### Data (Raw) ################# 
+    file_path = "the-verdict.txt"
+    url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
+
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode('utf-8')
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+    else:
+        with open(file_path, "r", encoding="utf-8") as file:
+            text_data = file.read()
+
+    print(text_data[:99])
+
+    # Train/validation ratio
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+
+    ### Dataloader #####################
+
+    train_loader = create_dataloader_v1(
+        train_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+    val_loader = create_dataloader_v1(
+        val_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
+
+    ### Model, Tokenizer, Optimizer.
     model = GPTModel(GPT_CONFIG_124M)
     model.to(device)
+    tokenizer = tiktoken.get_encoding("gpt2")
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
 
+    ### Train Loop.
     num_epochs = 10
     train_losses, val_losses, tokens_seen = train_model_simple(
-        model, train_loader, val_loader, optimizer, device,
-        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
-        start_context="Every effort moves you", tokenizer=tokenizer
+        model, 
+        train_loader, 
+        val_loader, 
+        optimizer, 
+        device,
+        num_epochs=num_epochs, 
+        eval_freq=5, 
+        eval_iter=5,
+        start_context="Every effort moves you", 
+        tokenizer=tokenizer
     )
 
     # Note:
     # Uncomment the following code to show the execution time
-    # end_time = time.time()
-    # execution_time_minutes = (end_time - start_time) / 60
-    # print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+    end_time = time.time()
+    execution_time_minutes = (end_time - start_time) / 60
+    print(f"Training completed in {execution_time_minutes:.2f} minutes.")
